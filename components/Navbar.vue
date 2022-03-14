317f0ee6910ce0e8cececf188ba8d6e046d32824
Paul Heng <rahveiz@gmail.com>
2022-03-14 21:15:33 +0100

    Develop (#108)
    * chore(deps-dev): bump postcss from 8.3.9 to 8.3.11 (#54)
    
    * fix(deps): bump @nuxt/content from 1.14.0 to 1.15.0 (#55)
    
    * fix(deps): bump core-js from 3.18.2 to 3.19.1 (#60)
    
    * chore(deps-dev): bump eslint-plugin-nuxt from 2.0.0 to 3.0.0 (#56)
    
    Bumps [eslint-plugin-nuxt](https://github.com/nuxt/eslint-plugin-nuxt) from 2.0.0 to 3.0.0.
    - [Release notes](https://github.com/nuxt/eslint-plugin-nuxt/releases)
    - [Changelog](https://github.com/nuxt/eslint-plugin-nuxt/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/nuxt/eslint-plugin-nuxt/compare/v2.0.0...v3.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint-plugin-nuxt
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix(deps): bump @nuxt/content from 1.15.0 to 1.15.1 (#62)
    
    Bumps [@nuxt/content](https://github.com/nuxt/content) from 1.15.0 to 1.15.1.
    - [Release notes](https://github.com/nuxt/content/releases)
    - [Changelog](https://github.com/nuxt/content/blob/main/docs/CHANGELOG.md)
    - [Commits](https://github.com/nuxt/content/compare/@nuxt/content@1.15.0...@nuxt/content@1.15.1)
    
    ---
    updated-dependencies:
    - dependency-name: "@nuxt/content"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump eslint-plugin-nuxt from 3.0.0 to 3.1.0 (#67)
    
    Bumps [eslint-plugin-nuxt](https://github.com/nuxt/eslint-plugin-nuxt) from 3.0.0 to 3.1.0.
    - [Release notes](https://github.com/nuxt/eslint-plugin-nuxt/releases)
    - [Changelog](https://github.com/nuxt/eslint-plugin-nuxt/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/nuxt/eslint-plugin-nuxt/compare/v3.0.0...v3.1.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint-plugin-nuxt
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * update packages
    
    * lint
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,7 @@
 <template>
   <nav class="bg-white dark:bg-gray-700 shadow mb-3 py-3">
     <div
-      class="
-        flex flex-row flex-wrap
-        justify-center
-        gap-3
-        sm:gap-0 sm:justify-between
-        items-center
-        sm:px-6
-      "
+      class="flex flex-row flex-wrap justify-center gap-3 sm:gap-0 sm:justify-between items-center sm:px-6"
     >
       <h1 class="text-lg order-first">
         <nuxt-link to="/"> paul-heng.fr </nuxt-link>
@@ -20,13 +13,7 @@
           v-for="link of links"
           :key="link.name"
           :to="link.href"
-          class="
-            px-3
-            py-2
-            mx-4
-            border-b-4 border-transparent
-            hover:border-red-200
-          "
+          class="px-3 py-2 mx-4 border-b-4 border-transparent hover:border-red-200"
         >
           {{ link.title }}
         </nuxt-link>
--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -4,35 +4,11 @@
     @click="toggleColorMode"
   >
     <div
-      class="
-        w-16
-        h-10
-        flex
-        items-center
-        bg-gray-300
-        rounded-full
-        p-1
-        duration-300
-        ease-in-out
-      "
+      class="w-16 h-10 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out"
       :class="{ 'bg-green-400': isDark }"
     >
       <div
-        class="
-          bg-white
-          w-8
-          h-8
-          rounded-full
-          shadow-md
-          transform
-          duration-300
-          ease-in-out
-          text-black
-          flex
-          justify-center
-          items-center
-          text-lg
-        "
+        class="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out text-black flex justify-center items-center text-lg"
         :class="{ 'translate-x-6': isDark }"
       >
         <FontAwesomeIcon :icon="['fas', switcherIcon]" />
